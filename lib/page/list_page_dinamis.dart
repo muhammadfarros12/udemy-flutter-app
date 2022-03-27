33c1402f15db6761629e84960e884ba89be5a08c
muhammad farroos <muhammadfarroos12@gmail.com>
2022-03-27 21:07:35 +0700

    [commit 3] List Page Dinamis

--- lib/page/list_page_dinamis.dart
+++ lib/page/list_page_dinamis.dart
@@ -26,31 +26,52 @@ class _ListPageDinamisState extends State<ListPageDinamis> {
     'pepaya'
   ];
 
+  void addItem() {
+    _fruits.add('Buah');
+    setState(() {});
+  }
+
+  void deleteItem(int indexItem) {
+    _fruits.removeAt(indexItem);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('List Page Dinamis'),
+        title: const Text('List Page Dinamis'),
         titleSpacing: 0,
       ),
-      body: ListView.builder(
-        itemCount: _fruits.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: CircleAvatar(
-              child: Text('${index + 1}'),
-            ),
-            title: Text(_fruits[index]),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.delete, color: Colors.red),
-            ),
-          );
-        },
+      body: Scrollbar(
+        // listView.builder digunakan ketika kita ingin membuat list secara dinamis,
+        // jika hanya menggunakan ListView saja, maka kita akan membuat list satu persatu
+        // ListView.separated dapat digunakan ketika kita ingin membuat pembatas didalam list tersebut
+        child: ListView.separated(
+          separatorBuilder: (context, index) => const Divider(
+            indent: 20,
+            height: 10,
+            endIndent: 10,
+            color: Colors.amber,
+          ),
+          itemCount: _fruits.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: CircleAvatar(
+                child: Text('${index + 1}'),
+              ),
+              title: Text(_fruits[index]),
+              trailing: IconButton(
+                onPressed: () => deleteItem(index),
+                icon: const Icon(Icons.delete, color: Colors.red),
+              ),
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
+        onPressed: () => addItem(),
+        child: const Icon(Icons.add),
       ),
     );
   }
