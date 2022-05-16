f626f053a1adf4764300c7b59f065c32f509c042
muhammad farroos <muhammadfarroos12@gmail.com>
2022-05-16 11:01:35 +0700

    Action {commit 2] Dismissable

--- lib/page/interaction/action_page.dart
+++ lib/page/interaction/action_page.dart
@@ -2,9 +2,31 @@
 
 import 'package:flutter/material.dart';
 
-class ActionPage extends StatelessWidget {
+class ActionPage extends StatefulWidget {
   const ActionPage({Key? key}) : super(key: key);
 
+  @override
+  State<ActionPage> createState() => _ActionPageState();
+}
+
+class _ActionPageState extends State<ActionPage> {
+  final List _fruits = [
+    'semangka',
+    'pisang',
+    'anggur',
+    'mangga',
+    'durian',
+    'kiwi',
+    'melon',
+    'buah naga',
+    'sirsak',
+    'jambu',
+    'lemon',
+    'nanas',
+    'jambu batu',
+    'kelengkeng',
+    'pepaya'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,22 +38,44 @@ class ActionPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(16),
           child: Column(children: [
-            Material(
-              borderRadius: BorderRadius.circular(30),
-              color: Colors.amberAccent,
-              child: InkWell(
-                borderRadius: BorderRadius.circular(30),
-                splashColor: Colors.blue,
-                onTap: () {
-                  print('custom button clicked');
-                },
-                child: const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                  child: Text('Custom Button dscv erklbvgt bjtr bkfjebntl'),
-                ),
-              ),
-            ),
+            customButton(),
+            ListView.builder(
+                itemCount: _fruits.length,
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return Dismissible(
+                    key: Key(_fruits[index]),
+                    background: Container(
+                      color: Colors.amber[300],
+                    ),
+                    onDismissed: (direction) {
+                      setState(() => _fruits.removeAt(index));
+                    },
+                    child: ListTile(
+                      title: Text('${index + 1}. ' + _fruits[index]),
+                    ),
+                  );
+                })
           ]),
+        ),
+      ),
+    );
+  }
+
+  Material customButton() {
+    return Material(
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.amberAccent,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(30),
+        splashColor: Colors.blue,
+        onTap: () {
+          print('custom button clicked');
+        },
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+          child: Text('Custom Button'),
         ),
       ),
     );
